64d7a3cf66a3fd98baa5f340d677c07f4179b99f
Charlie Draper <charlie.draper@soprasteria.com>
2018-02-01 19:49:48 +0000

    removed old components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { WeatherDisplayComponent } from './weather-display/weather-display.component';
-import { CityComponent } from './city/city.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WeatherDisplayComponent,
-    CityComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
