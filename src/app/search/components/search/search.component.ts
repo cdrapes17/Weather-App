6ed5ec5abcc742cb2fd86672507771594aae8c7d
Charlie Draper <charlie.draper@soprasteria.com>
2018-02-03 14:01:57 +0000

    cleaned css of form

--- src/app/search/components/search/search.component.ts
+++ src/app/search/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, AbstractControl } from '@angular/forms';
 import { WeatherInformationService } from '../../../weather-information/services/weather-information.service';
 import { SearchService } from '../../services/search.service';
@@ -11,7 +11,9 @@ import { SearchService } from '../../services/search.service';
 export class SearchComponent implements OnInit {
 
   public SearchForm: FormGroup;
+  @ViewChild('cityInput') cityInput: ElementRef;
   constructor(private _fb: FormBuilder, private searchService: SearchService) { }
+  
 
   ngOnInit() {
     this.SearchForm = this._fb.group({
@@ -21,6 +23,11 @@ export class SearchComponent implements OnInit {
 
   getWeatherFromCity(payload) {
     this.searchService.getWeather(payload.searchCity);
+    this.clearInput(this.cityInput)
+  }
+
+  clearInput(element: ElementRef){
+    element.nativeElement.value = '';
   }
 
 }
